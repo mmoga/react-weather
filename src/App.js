a5a0e9f423fa439dcde0629b4a52dac1b6164233
Matthew Mogavero <matt.moga@gmail.com>
2018-02-21 08:06:10 -0500

    updated the latitude and longitude inputs to handle state changes with event handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 0,
+      lon: 0
+    };
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+  }
+  handleLatChange(e) {
+    this.setState({
+      lat: +e.target.value
+    });
+  }
+  handleLonChange(e) {
+    this.setState({
+      lon: +e.target.value
+    });
+  }
   render() {
     return (
       <div>
-        TEST
+        <h1>React Weather</h1>
+        <p>Enter your latitude and longitude to get the weather!</p>
+        <form>
+          <label>
+            Latitude:
+            <input
+              type="text"
+              onChange={e => this.handleLatChange(e)}
+              value={this.state.lat}
+            />
+          </label>
+          <label>
+            Longitude:
+            <input
+              type="text"
+              onChange={e => this.handleLonChange(e)}
+              value={this.state.lon}
+            />
+          </label>
+          <button type="submit">Get The Weather!</button>
+        </form>
       </div>
     );
   }
