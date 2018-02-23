4363cb114e553c14da16b5e32a427c1743ee656a
Matthew Mogavero <matt.moga@gmail.com>
2018-02-22 19:38:30 -0500

    fixed longitude typo

--- client/src/services/weather.js
+++ client/src/services/weather.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const getWeather = (latitude, longitude) => {
-    const url =`/forecast/${latitude},${latitude}`;
+    const url =`/forecast/${latitude},${longitude}`;
     return axios.get(url);
 }
